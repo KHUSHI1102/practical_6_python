61322cf0d8c6ba543f8685de7b98370f54cbedae
KHUSHI1102 <96758617+KHUSHI1102@users.noreply.github.com>
2022-02-12 19:25:22 +0530

    Update 20ce142_pre_6.py

--- 20ce142_pre_6.py
+++ 20ce142_pre_6.py
@@ -1,12 +1,12 @@
 '''NAME : khushi tala
    ID   : 20Ce142
 '''
-from collections import Counter #import counter
-n=int(input ()) #take number of test cases from user
-l=list() #list of string
+from collections import Counter    #import counter
+n=int(input ())                    #take number of test cases from user
+l=list()                           #list of string
 for _ in range (n):
-    l.append(input()) #add a string to existing string
-x=Counter(l) #count the items in a iterable
+    l.append(input())              #add a string to existing string
+x=Counter(l)                       #count the items in a iterable
 
 print (len(x))
-print(*x.values()) #Unpacking a items using Asterisk operator
+print(*x.values())                 #Unpacking a items using Asterisk operator
